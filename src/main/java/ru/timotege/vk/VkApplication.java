23b47aece0a410fcff30ce641b973cd2038e48c9
t1mtg <timagurman@gmail.com>
2022-11-12 20:25:41 +0300

    feat: add feign annotation to VkApplication

--- src/main/java/ru/timotege/vk/VkApplication.java
+++ src/main/java/ru/timotege/vk/VkApplication.java
@@ -2,8 +2,10 @@ package ru.timotege.vk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VkApplication {
 
 	public static void main(String[] args) {
